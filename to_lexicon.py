4292d8329a328cc01bdb057cdaa98fc8b4cbec33
Nina <ninawang526@gmail.com>
2018-12-22 21:07:39 -0500

    overlap

--- to_lexicon.py
+++ to_lexicon.py
@@ -9,6 +9,7 @@ from nltk import tokenize
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
 from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.porter import *
 
 import gensim
 from gensim import corpora
@@ -53,9 +54,12 @@ def filter_support_words(article):
         
 
 ## helper method for text cleanup
-def text_cleanup(text, filter_support = False):
+def text_cleanup(text, filter_support=False):
 	def get_lemma(word):
 		return WordNetLemmatizer().lemmatize(word)
+	def get_stem(word):
+		stemmer = PorterStemmer()
+		return stemmer.stem(word)
 
 	stop_words = set(stopwords.words('english'))
 	months = ["january","february","march","april","may","june","july","august","september","october","november","december"]
@@ -68,37 +72,63 @@ def text_cleanup(text, filter_support = False):
 	tokens = [token for token in tokens if token.lower() not in stop_words]
 	tokens = [token for token in tokens if token.lower() not in months]
 	tokens = [get_lemma(token.lower()) for token in tokens]
+	tokens = [get_stem(token) for token in tokens]
 	return tokens
 
 
 # the 15 words around a sentence
 def get_collocations(articles, keywords, opposite_keywords,exclude=[]): 
+	stemmer = PorterStemmer() 
+
+	needles = set([stemmer.stem(s.lower()) for s in keywords])
+	anti_needles = set([stemmer.stem(s.lower()) for s in (opposite_keywords+exclude)])
+
+	collocations = []
+	for article in articles:
+		title, date_publish, text = article.title, article.date_publish, article.text
+		sentences = tokenize.sent_tokenize(text)
+		words = text_cleanup(text,filter_support=True)
+		# tokened_s = [text_cleanup(sent, filter_support=True) for sent in sentences]
+
+		# for n, sent in enumerate(tokened_s):
+		#     haystack = set([s.lower() for s in sent])
+		#     if len(haystack.intersection(needles)) > 0 and len(haystack.intersection(anti_needles)) == 0:
+		#         collocations += sent
+		for n, word in enumerate(words):
+			if word in needles:
+				start_i = max(0, n-15)
+				end_i = min(n+15, len(words)-1)
+				surround = words[start_i:end_i]
+
+				haystack = set([s.lower() for s in surround])
+				if len(haystack.intersection(anti_needles)) == 0:
+					collocations += [w for w in surround if w.lower() not in needles and w.lower() not in ["presid","parti"]]
+
+	# collocations = [word for word in collocations if word.lower() not in keywords and not in exclude]
+	return collocations
+
+
+# collocations, except preserving article order
+def tokenize_articles(articles, keywords, opposite_keywords, exclude=[]):
     needles = set([s.lower() for s in keywords])
     anti_needles = set([s.lower() for s in (opposite_keywords+exclude)])
 
     collocations = []
-    # sentences = []
     for article in articles:
         title, date_publish, text = article.title, article.date_publish, article.text
-        # sentences = tokenize.sent_tokenize(text)
-        words = text_cleanup(text,filter_support=True)
-        # tokened_s = [text_cleanup(sent, filter_support=True) for sent in sentences]
+        sentences = tokenize.sent_tokenize(text)
+        tokened_s = [text_cleanup(sent, filter_support=True) for sent in sentences]
 
-        # for n, sent in enumerate(tokened_s):
-        #     haystack = set([s.lower() for s in sent])
-        #     if len(haystack.intersection(needles)) > 0 and len(haystack.intersection(anti_needles)) == 0:
-        #         collocations += sent
-        for n, word in enumerate(words):
-        	if word in needles:
-	        	start_i = max(0, n-15)
-	        	end_i = min(n+15, len(words)-1)
-	        	surround = words[start_i:end_i]
-	        	
-	        	haystack = set([s.lower() for s in surround])
-	        	if len(haystack.intersection(anti_needles)) == 0:
-	        		collocations += [w for w in surround if w.lower() not in needles and w.lower() not in ["president","party"]]
-	    
-    # collocations = [word for word in collocations if word.lower() not in keywords and not in exclude]
+        art = []
+        for n, sent in enumerate(tokened_s):
+            haystack = set([s.lower() for s in sent])
+            if len(haystack.intersection(needles)) > 0 and len(haystack.intersection(anti_needles)) == 0 and len(haystack.intersection(set(exclude))) == 0:
+                sent = [word for word in sent if word not in keywords]
+                art += sent
+        
+        if len(art)>0:
+        	collocations.append(art)
+
     return collocations
 
 

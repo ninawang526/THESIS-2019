245ea6f239930eb5005537a6113e710e9be6e72b
Nina Wang <ninawang@nat-oitwireless-inside-vapornet100-10-9-78-151.princeton.edu>
2019-03-19 13:52:30 -0400

    dtm python

--- ChangeOverTime/dtm.sh
+++ ChangeOverTime/dtm.sh
@@ -3,9 +3,9 @@
   --mode=fit \
   --rng_seed=0 \
   --initialize_lda=true \
-  --corpus_prefix=example/test \
-  --outname=example/model_run \
-  --top_chain_var=0.005 \
+  --corpus_prefix=../../ChangeOverTime/data/doc \
+  --outname=../../ChangeOverTime/data/model_run \
+  --top_chain_var=0.1 \
   --alpha=0.01 \
   --lda_sequence_min_iter=6 \
   --lda_sequence_max_iter=20 \
@@ -13,4 +13,3 @@
 
 
 
-  
--- ChangeOverTime/dtm_utils.py
+++ ChangeOverTime/dtm_utils.py
@@ -2,18 +2,16 @@
 # def create_documents(col):
 
 
-import sys
-p = "/Users/ninawang/Thesis/remote"
-if p not in sys.path:
-	sys.path.append(p)
-	sys.path.append("/usr/local/lib/python3.7/site-packages")
+# import sys
+# p = "/Users/ninawang/Thesis/remote/THESIS2019/"
+# if p not in sys.path:
+# 	sys.path.append(p)
+# 	sys.path.append("/usr/local/lib/python3.7/site-packages")
 
-print (sys.path)
+# print (sys.path)
 
-
-
-import THESIS2019.to_lexicon as lex
-from THESIS2019.base_words import *
+import to_lexicon as lex
+from base_words import *
 import datetime
 import pickle
 
@@ -23,15 +21,18 @@ from gensim.models import LdaSeqModel
 from gensim.corpora import Dictionary, bleicorpus
 import os, numpy
 
+# slow python-wrapped model
 def ldaseq():
-	path = "/Users/ninawang/Thesis/remote/THESIS2019/NYT-OPINION2012-2013-processed/"
+	print("starting")
+
+	path = "/n/fs/thesis-ninaw/ARTICLES-2012/processed/NYT-OPINION2012-2013-processed/"
 
 	start = datetime.datetime(2012, 6, 1)
 	end = datetime.datetime(2013, 7, 1)
 
 	articles2012 = lex.get_articles_from_filepath(path,start,end)
 
-	path = "/Users/ninawang/Thesis/remote/THESIS2019/NYT-OPINION2016-2017-processed/"
+	path = "/n/fs/thesis-ninaw/ARTICLES-2016/processed/NYT-OPINION2016-2017-processed/"
 
 	start = datetime.datetime(2016, 6, 1)
 	end = datetime.datetime(2017, 7, 1)
@@ -75,5 +76,71 @@ def ldaseq():
 		pickle.dump(ldaseq, f)
 
 
+def get_doc_and_timeslices(col):
+    total = [d for d in col if d[0] is not None]
+    docs = [d[1] for d in total]
+    timeslices = [d[0].year for d in total]
+    return docs, timeslices
+
+# alter topic evolutionwith the parameter "top_chain_var", default=0.005
+def to_document_dat():
+	# load 2008
+	with open("../Jupyter Notebooks/2008-collocations-dem.pkl", "rb") as f:
+		dem2008 = pickle.load(f)
+	with open("../Jupyter Notebooks/2008-collocations-rep.pkl", "rb") as f:
+		rep2008 = pickle.load(f)
+
+	# load 2012
+	with open("../Jupyter Notebooks/2012-collocations-dem.pkl", "rb") as f:
+		dem2012 = pickle.load(f)
+	with open("../Jupyter Notebooks/2012-collocations-rep.pkl", "rb") as f:
+		rep2012 = pickle.load(f)
+
+	# load 2016
+	with open("../Jupyter Notebooks/2016-collocations-dem.pkl", "rb") as f:
+		dem2016 = pickle.load(f)
+	with open("../Jupyter Notebooks/2016-collocations-rep.pkl", "rb") as f:
+		rep2016 = pickle.load(f)
+
+	dems_docs_2008 = [d[1] for d in dem2008]
+	dems_docs_2012 = [d[1] for d in dem2012]
+	dems_docs_2016 = [d[1] for d in dem2016]
+	combined = dems_docs_2008+dems_docs_2012+dems_docs_2016
+
+	print("2008:%d\n2012:%d\n2016:%d\n"%(len(dems_docs_2008),len(dems_docs_2012),len(dems_docs_2016)))
+	print("total:%d\n"%(len(combined)))
+
+	dct = Dictionary(combined)
+	
+	# write dictionary
+	with open("data/dictionary.pkl","wb") as f:
+		pickle.dump(dct,f)
+
+	# write document file
+	with open("data/doc-mult.dat", "w") as f:
+		for text in combined:
+			bow = dct.doc2bow(text)
+			row=str(len(bow))+" "+(" ").join([str(idx)+":"+str(c) for idx,c in bow])+"\n"
+			f.write(row)
+
+	# write timestamp file
+	with open("data/doc-seq.dat", "w") as f:
+		f.write("3\n"+str(len(dems_docs_2008))+"\n"+str(len(dems_docs_2012))+"\n"+str(len(dems_docs_2016)))
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-	ldaseq()
+	# ldaseq()
+	to_document_dat()
+
+
+
+
+
